f87309e6659c066dbfbb672c82595fa2e8f5c0e5
elnijs <Elias.Nijs@UGent.be>
2022-04-09 14:55:06 +0200

    project startup

---
+++ Squared2/app/src/main/java/sel/group9/squared2/MainActivity.kt
@@ -0,0 +1,32 @@
+package sel.group9.squared2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import sel.group9.squared2.ui.theme.Squared2Theme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      Squared2Theme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+          TitleComponent();
+        }
+      }
+    }
+  }
+    
+}
+
+@Composable
+fun TitleComponent() {
+  Text("Squared2");
+}
