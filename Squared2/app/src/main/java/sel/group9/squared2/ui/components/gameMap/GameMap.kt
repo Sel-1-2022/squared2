5749f7bc3104141724e0d87a91d5bb363c53f70a
Aaron Vranken <23616042+AaronVr@users.noreply.github.com>
2022-04-20 16:32:23 +0200

    Something is wrong

--- Squared2/app/src/main/java/sel/group9/squared2/ui/components/gameMap/GameMap.kt
+++ Squared2/app/src/main/java/sel/group9/squared2/ui/components/gameMap/GameMap.kt
@@ -1,22 +1,21 @@
 package sel.group9.squared2.ui.components.gameMap
 
-import android.content.Context
-import android.location.LocationManager
-import android.location.LocationRequest
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Looper
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.content.ContextCompat.getSystemService
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 import sel.group9.squared2.ui.theme.SquaredTheme
@@ -49,10 +48,36 @@ fun GameMap(fusedLocationClient: FusedLocationProviderClient) {
     }
     var centerToggle by remember { mutableStateOf(true) }
 
-//    fusedLocationClient()
+    val context = LocalContext.current
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult?) {
+            Log.v("LocationTest", "Location: In location callback")
+            locationResult ?: return
+            for (location in locationResult.locations) {
+                Log.v("LocationTest", "Location: ${location.latitude}, ${location.longitude}")
+                cameraPositionState.move(CameraUpdateFactory.newLatLng(LatLng(location.latitude, location.longitude)))
+            }
+        }
+    }
+
+
+
+    fun requestLocationUpdates() {
+        val locationRequest = LocationRequest.create()
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.e("LocationError", "Fuckedd")
+            return
+        }
+        Log.v("LocationTest", "In request Location updates.")
+        if (centerToggle) fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+    }
+
 
     fun onReady() {
         cameraPositionState.move(CameraUpdateFactory.newLatLngZoom(cameraPositionState.position.target, 18.0f))
+        requestLocationUpdates()
     }
 
     AskMapsPermission {
