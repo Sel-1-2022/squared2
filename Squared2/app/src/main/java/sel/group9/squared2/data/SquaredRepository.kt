ac76d4edf0cbd0a47be2c63a0f90c94f02300493
Bavo <bavo.verstraeten@ugent.be>
2022-04-20 11:30:30 +0200

    all api methods possible in repository

--- Squared2/app/src/main/java/sel/group9/squared2/data/SquaredRepository.kt
+++ Squared2/app/src/main/java/sel/group9/squared2/data/SquaredRepository.kt
@@ -1,12 +1,32 @@
 package sel.group9.squared2.data
 
+import android.util.Log
 import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class SquaredRepository@Inject constructor(val backend: Backend, val settings: Settings) {
+class SquaredRepository@Inject constructor(private val backend: Backend, private val settings: Settings) {
+
+    suspend fun postUser(name:String,color:String,lat:Int,long:Int):String{
+        return backend.postUser(name,color,lat,long)
+    }
+
+    suspend fun getUser(id:String): User{
+        return backend.getUser(id)
+    }
+
+    suspend fun patchUser(id:String,name:String?=null,color:String?=null,lat:Int?=null,long:Int?=null,last:Int?=null):User{
+        return backend.patchUser(id,name,color,lat,long,last)
+    }
+
+    suspend fun nearbyUser(lat:Int,long:Int,dist:Int):List<User>{
+        return backend.nearbyUsers(lat,long,dist)
+    }
+
     fun getColor():StateFlow<Color>{
         return settings.getColor()
     }
