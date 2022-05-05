b2f4e6e43c53d6a4916821be47b02217f60a0d2a
AaronVr <23616042+AaronVr@users.noreply.github.com>
2022-05-05 17:57:02 +0200

    Trying to get tests to work

--- Squared2/app/src/androidTest/java/sel/group9/squared2/UI/StartScreen/ButtonCallbackTests.kt
+++ Squared2/app/src/androidTest/java/sel/group9/squared2/UI/StartScreen/ButtonCallbackTests.kt
@@ -1,10 +1,16 @@
 package sel.group9.squared2.UI.StartScreen
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.kotlin.atLeastOnce
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 import sel.group9.squared2.ui.screens.StartScreen
 import sel.group9.squared2.ui.theme.SquaredTheme
+import sel.group9.squared2.ui.theme.warmYellow
 
 class ButtonCallbackTests {
     @get:Rule
@@ -14,12 +20,30 @@ class ButtonCallbackTests {
     fun testOnClickSettingsCallsCallback() {
         val name = "name"
 
+        val onChangeTest = mock<(String)->Unit>()
+        val onLoginTest = mock<()->Unit>()
+        val onColorTest = mock<()->Unit>()
+        val onSettingsTest = mock<()->Unit>()
+
         composeTestRule.setContent {
             SquaredTheme {
-                StartScreen(name, )
+                StartScreen(
+                    name,
+                    onChange=onChangeTest,
+                    onStart = onLoginTest,
+                    onColorPressed = onColorTest,
+                    onCogPressed = onSettingsTest,
+                    color = warmYellow
+                )
             }
         }
-        val test = Mock<(String)->Unit>()
+
+        composeTestRule
+            .onNodeWithContentDescription("Settings")
+            .performClick()
+
+        composeTestRule.waitForIdle()
+        verify(onSettingsTest, atLeastOnce())
     }
 
 
